e1e26c7445fb6b9596db0b7b9d5ef956aa5f1863
ikexing-cn <ikexing@qq.com>
2023-03-05 15:34:10 +0800

    chore: first to search for JAVA_HOME in the environment variables and then catch any errors.

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,15 +1,15 @@
-import * as path from 'path';
-import * as child_process from 'child_process';
-import {ExtensionContext, window, workspace} from 'vscode';
-import {LanguageClient, LanguageClientOptions, ServerOptions} from 'vscode-languageclient/node';
+import { join } from 'node:path';
+import { execSync } from 'node:child_process';
+import { ExtensionContext, window, workspace } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 
 export function activate(context: ExtensionContext) {
 	let logChannel = window.createOutputChannel('ZenScript Language Server', "log");
 
-	logChannel.appendLine('[Extension] Starting ZenScript language server')
+	logChannel.appendLine('[Extension] Starting ZenScript language server');
 	getJavaHome().then(javahome => {
-		let javabin: string = path.join(javahome, 'bin', 'java');
-		let classpath: string = path.join(__dirname, '..', '*');
+		let javabin: string = join(javahome, 'bin', 'java');
+		let classpath: string = join(__dirname, '..', '*');
 		let args: string[] = ['-cp', classpath];
 		let main = 'raylras.zen.langserver.StandardIOLauncher';
 		let debug = '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005,quiet=y';
@@ -18,7 +18,7 @@ export function activate(context: ExtensionContext) {
 		logChannel.appendLine(`[Extension] Class path: ${classpath}`)
 		logChannel.appendLine(`[Extension] Main class: ${main}`)
 
-		if(workspace.getConfiguration().get('zenscript.languageServer.debug') == true) {
+		if (workspace.getConfiguration().get('zenscript.languageServer.debug') == true) {
 			logChannel.appendLine('[Extension] Language server is starting on debug mode');
 			logChannel.appendLine(`[Extension] Arguments: ${debug}`);
 			args.push(debug);
@@ -42,13 +42,19 @@ export function activate(context: ExtensionContext) {
 		let client = new LanguageClient('ZenScript Language Client', serverOptions, clientOptions);
 		let disposable = client.start();
 		// context.subscriptions.push(disposable);
+	}).catch(error => {
+		logChannel.appendLine('[Extension] Failed to start ZenScript language server');
+		logChannel.appendLine(error?.message || error);
 	})
 
 }
 
-export function deactivate() {}
+export function deactivate() { }
 
 function getJavaHome() {
+	let javaHome = process.env?.['JAVA_HOME']
+	if (javaHome) return Promise.resolve(javaHome)
+
 	let cmd: string;
 	if (process.platform == 'win32') {
 		cmd = 'java -XshowSettings:properties -version 2>&1 | findstr "java.home"';
@@ -58,7 +64,7 @@ function getJavaHome() {
 
 	return new Promise<string>((resolve, reject) => {
 		try {
-			let response = child_process.execSync(cmd).toString();
+			let response = execSync(cmd).toString();
 			resolve(response.split('java.home =')[1].trim());
 		} catch (error) {
 			reject(error)
