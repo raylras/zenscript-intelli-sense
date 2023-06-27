8741dfaad011ee3b7e5326b68705e0db97309f0d
raylras <raylras@outlook.com>
2023-06-28 00:05:07 +0800

    feat(workspace): ProbeZS tips

--- src/main/java/raylras/zen/langserver/WorkspaceManager.java
+++ src/main/java/raylras/zen/langserver/WorkspaceManager.java
@@ -2,7 +2,9 @@ package raylras.zen.langserver;
 
 import raylras.zen.code.CompilationEnvironment;
 import raylras.zen.code.CompilationUnit;
+import raylras.zen.util.Logger;
 import raylras.zen.util.Utils;
+import raylras.zen.util.l10n.L10N;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WorkspaceManager {
+
+    private static final Logger logger = Logger.getLogger("workspace");
 
     private final List<Path> workspaceList = new ArrayList<>();
     private final List<CompilationEnvironment> compilationList = new ArrayList<>();
@@ -53,6 +57,10 @@ public class WorkspaceManager {
             compilationRoot = documentPath;
         CompilationEnvironment env = new CompilationEnvironment(compilationRoot);
         env.load();
+        if (!hasDzsFile(env)) {
+            logger.logInfo("Could not find *.d.zs files in project environment: {0}", env);
+            logger.showInfo(L10N.getString("environment.dzs_not_found"));
+        }
         compilationList.add(env);
     }
 
@@ -64,6 +72,15 @@ public class WorkspaceManager {
         for (Path workspace : workspaceList) {
             if (Utils.isSubPath(workspace, documentPath))
                 return true;
+        }
+        return false;
+    }
+
+    private boolean hasDzsFile(CompilationEnvironment env) {
+        for (CompilationUnit unit : env.getUnits()) {
+            if (unit.isDzs()) {
+                return true;
+            }
         }
         return false;
     }
