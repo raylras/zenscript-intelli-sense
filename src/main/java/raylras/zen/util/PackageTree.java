abfc34c73e3853c8fb82c8e52839db6f7ab03957
youyihj <youyi580@qq.com>
2023-08-10 18:03:36 +0800

    complete import

---
+++ src/main/java/raylras/zen/util/PackageTree.java
@@ -0,0 +1,46 @@
+package raylras.zen.util;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class PackageTree<V> {
+    private final Map<String, PackageTree<V>> subTrees = new HashMap<>();
+    private V element;
+    
+    public static <V> PackageTree<V> of(Map<String, V> map) {
+        PackageTree<V> tree = new PackageTree<>();
+        map.forEach(tree::put);
+        return tree;
+    }
+
+    public void put(String path, V value) {
+        PackageTree<V> leaf = this;
+        Function<String, PackageTree<V>> treeCreator = it -> new PackageTree<>();
+        for (String s : path.split("\\.")) {
+            leaf = leaf.subTrees.computeIfAbsent(s, treeCreator);
+        }
+        leaf.element = value;
+    }
+
+    public V getElement() {
+        return element;
+    }
+
+    public Collection<String> getSubTrees() {
+        return subTrees.keySet();
+    }
+
+    public PackageTree<V> get(String path) {
+        PackageTree<V> node = this;
+        for (String s : path.split("\\.")) {
+            node = node.subTrees.get(s);
+            if (node == null) {
+                return PackageTree.of(Collections.emptyMap());
+            }
+        }
+        return node;
+    }
+}
