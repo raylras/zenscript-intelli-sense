7053c8be898a7205ae414f72670db5fdee0a3894
raylras <raylras@outlook.com>
2023-08-17 20:53:02 +0800

    refactor(ClassSymbol): remove field 'qualifiedName'

--- src/main/java/raylras/zen/code/symbol/ClassSymbol.java
+++ src/main/java/raylras/zen/code/symbol/ClassSymbol.java
@@ -5,6 +5,7 @@ import raylras.zen.code.annotation.Annotation;
 import raylras.zen.code.parser.ZenScriptParser.ClassDeclarationContext;
 import raylras.zen.code.scope.Scope;
 import raylras.zen.code.type.ClassType;
+import raylras.zen.util.CSTNodes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,14 +14,10 @@ import java.util.Map;
 public class ClassSymbol extends Symbol {
 
     private final ClassType type;
-    private final String qualifiedName;
 
     public ClassSymbol(ClassDeclarationContext cst, CompilationUnit unit) {
         super(cst, unit);
         this.type = new ClassType(this);
-        String decalredName = cst.simpleNameOrPrimitiveType().getText();
-        String packageName = unit.getPackage();
-        this.qualifiedName = packageName.isEmpty() ? decalredName : packageName + "." + decalredName;
     }
 
     public List<Symbol> getMembers() {
@@ -50,7 +47,9 @@ public class ClassSymbol extends Symbol {
     }
 
     public String getQualifiedName() {
-        return qualifiedName;
+        String declaredName = getSimpleName();
+        String packageName = unit.getPackage();
+        return packageName.isEmpty() ? declaredName : packageName + "." + declaredName;
     }
 
     @Override
@@ -61,6 +60,11 @@ public class ClassSymbol extends Symbol {
     @Override
     public Kind getKind() {
         return Kind.CLASS;
+    }
+
+    @Override
+    public String getSimpleName() {
+        return CSTNodes.getText(getCst().simpleNameOrPrimitiveType());
     }
 
     @Override
