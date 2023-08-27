ae30b3db3c2dd9ccd98dd9483579871cb19037fb
raylras <raylras@outlook.com>
2023-08-27 16:31:35 +0800

    chore(vscode-extension): remove unused files

--- vscode-extension/build/dev.mjs
+++
@@ -1,7 +0,0 @@
-import { writeFileSync } from 'fs';
-import { resolve } from 'path'
-
-writeFileSync(
-    resolve('./client/out/extension.js'),
-    `module.exports = require('../src/extension.js')`
-);
