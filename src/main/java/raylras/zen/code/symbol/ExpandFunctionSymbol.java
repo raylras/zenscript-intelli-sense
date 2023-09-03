0f7726c37bcb4f886a66ef89c3ea2cc9525a390b
youyihj <youyi580@qq.com>
2023-09-03 23:49:34 +0800

    ExpandFunctionSymbol extends FunctionSymbol

--- src/main/java/raylras/zen/code/symbol/ExpandFunctionSymbol.java
+++ src/main/java/raylras/zen/code/symbol/ExpandFunctionSymbol.java
@@ -2,7 +2,7 @@ package raylras.zen.code.symbol;
 
 import raylras.zen.code.type.Type;
 
-public interface ExpandFunctionSymbol extends Symbol, Executable {
+public interface ExpandFunctionSymbol extends FunctionSymbol {
 
     Type getOwner();
 
