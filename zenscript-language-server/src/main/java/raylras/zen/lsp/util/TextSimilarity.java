fa3e79bf1db8ec0a9fdd15518e0123b0f0771929
raylras <raylras@outlook.com>
2023-09-25 21:46:48 +0800

    doc(TextSimilarity): add `isSubsequence` javadoc

--- zenscript-language-server/src/main/java/raylras/zen/lsp/util/TextSimilarity.java
+++ zenscript-language-server/src/main/java/raylras/zen/lsp/util/TextSimilarity.java
@@ -2,22 +2,32 @@ package raylras.zen.lsp.util;
 
 public class TextSimilarity {
 
+    /**
+     * Checks if {@code s} is a subsequence of {@code t}.
+     * @param s subsequence
+     * @param t string
+     * @return {@code true} if {@code s} is a subsequence of {@code t}
+     */
     public static boolean isSubsequence(String s, String t) {
-        if (s.isEmpty()) {
+        int sLen = s.length();
+        int tLen = t.length();
+
+        if (sLen == 0) {
             return true;
         }
 
         int i = 0;
         int j = 0;
-        while (i < s.length() && j < t.length()) {
+        while (i < sLen && j < tLen) {
             if (s.charAt(i) == t.charAt(j)) {
                 i++;
             }
 
             j++;
 
-            if (i == s.length())
+            if (i == sLen) {
                 return true;
+            }
         }
 
         return false;
