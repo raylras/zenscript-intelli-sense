61d91a34f29cdae95b59144120a8ca674c0ef1e4
youyihj <youyi580@qq.com>
2023-09-26 17:08:33 +0800

    switch to static import

--- zenscript-language-server/src/main/java/raylras/zen/lsp/provider/data/Snippet.java
+++ zenscript-language-server/src/main/java/raylras/zen/lsp/provider/data/Snippet.java
@@ -3,7 +3,7 @@ package raylras.zen.lsp.provider.data;
 import org.eclipse.lsp4j.*;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import raylras.zen.model.CompilationEnvironment;
-import raylras.zen.model.parser.ZenScriptParser;
+import raylras.zen.model.parser.ZenScriptParser.MemberAccessExprContext;
 import raylras.zen.model.symbol.Operator;
 import raylras.zen.model.type.*;
 import raylras.zen.util.Operators;
@@ -18,7 +18,7 @@ public interface Snippet {
 
     Optional<CompletionItem> get();
 
-    static Snippet createFor(Type type, CompilationEnvironment env, ZenScriptParser.MemberAccessExprContext memberAccessExprContext) {
+    static Snippet createFor(Type type, CompilationEnvironment env, MemberAccessExprContext memberAccessExprContext) {
         Type iteratorType = Operators.getUnaryOperatorResult(type, Operator.ITERATOR, env);
         if (iteratorType instanceof MapType) {
             return () -> Optional.of(createMemberCompletionItem(memberAccessExprContext, "for", "for key, value in map", "for $1, $2 in %s {\n\t$0\n}"));
@@ -28,7 +28,7 @@ public interface Snippet {
         return NONE;
     }
 
-    static Snippet createForI(Type type, CompilationEnvironment env, ZenScriptParser.MemberAccessExprContext memberAccessExprContext) {
+    static Snippet createForI(Type type, CompilationEnvironment env, MemberAccessExprContext memberAccessExprContext) {
         Type iteratorType = Operators.getUnaryOperatorResult(type, Operator.ITERATOR, env);
         if (iteratorType instanceof ListType) {
             return () -> Optional.of(createMemberCompletionItem(memberAccessExprContext, "fori", "for index, element in list", "for $1 in %s {\n\t$0\n}"));
@@ -36,22 +36,22 @@ public interface Snippet {
         return NONE;
     }
 
-    static Snippet createVal(ZenScriptParser.MemberAccessExprContext memberAccessExprContext) {
+    static Snippet createVal(MemberAccessExprContext memberAccessExprContext) {
         return () -> Optional.of(createMemberCompletionItem(memberAccessExprContext, "val", "val name = expr", "val $1 = %s;"));
     }
 
-    static Snippet createVar(ZenScriptParser.MemberAccessExprContext memberAccessExprContext) {
+    static Snippet createVar(MemberAccessExprContext memberAccessExprContext) {
         return () -> Optional.of(createMemberCompletionItem(memberAccessExprContext, "var", "var name = expr", "var $1 = %s;"));
     }
 
-    static Snippet createIfNull(Type type, ZenScriptParser.MemberAccessExprContext memberAccessExprContext) {
+    static Snippet createIfNull(Type type, MemberAccessExprContext memberAccessExprContext) {
         if (!(type instanceof NumberType || type == BoolType.INSTANCE || type == VoidType.INSTANCE)) {
             return () -> Optional.of(createMemberCompletionItem(memberAccessExprContext, "null", "if (isNull(expr))", "if (isNull(%s)) {\n\t$0\n}"));
         }
         return NONE;
     }
 
-    static Snippet createIfNotNull(Type type, ZenScriptParser.MemberAccessExprContext memberAccessExprContext) {
+    static Snippet createIfNotNull(Type type, MemberAccessExprContext memberAccessExprContext) {
         if (!(type instanceof NumberType || type == BoolType.INSTANCE || type == VoidType.INSTANCE)) {
             return () -> Optional.of(createMemberCompletionItem(memberAccessExprContext, "nn", "if (!isNull(expr))", "if (!isNull(%s)) {\n\t$0\n}"));
         }
@@ -59,7 +59,7 @@ public interface Snippet {
     }
 
 
-    private static CompletionItem createMemberCompletionItem(ZenScriptParser.MemberAccessExprContext memberAccessExprContext, String name, String description, String snippet) {
+    private static CompletionItem createMemberCompletionItem(MemberAccessExprContext memberAccessExprContext, String name, String description, String snippet) {
         CompletionItem item = new CompletionItem(name);
         item.setInsertTextMode(InsertTextMode.AdjustIndentation);
         item.setInsertTextFormat(InsertTextFormat.Snippet);
