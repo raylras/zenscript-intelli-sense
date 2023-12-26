031df8cdafb705a9e3a2c90dca11307eeea92b00
raylras <raylras@outlook.com>
2023-12-26 23:46:01 +0800

    fix(DocumentSymbolProvider): fix NPE caused by `ctx.simpleName()` being null

--- zenscript-language-server/src/main/kotlin/raylras/zen/lsp/provider/DocumentSymbolProvider.kt
+++ zenscript-language-server/src/main/kotlin/raylras/zen/lsp/provider/DocumentSymbolProvider.kt
@@ -13,76 +13,73 @@ import raylras.zen.util.textRange
 import raylras.zen.util.toLspRange
 
 object DocumentSymbolProvider {
-    fun documentSymbol(unit: CompilationUnit?, params: DocumentSymbolParams): List<DocumentSymbol>? {
-        unit ?: return null
+    fun documentSymbol(unit: CompilationUnit?, params: DocumentSymbolParams): List<DocumentSymbol> {
         val visitor = DocumentSymbolVisitor()
-        unit.parseTree?.accept(visitor)
+        unit?.parseTree?.accept(visitor)
         return visitor.topLevelSymbolList
     }
 
-    private class DocumentSymbolVisitor : Visitor<DocumentSymbol?>() {
+    private class DocumentSymbolVisitor : Visitor<Unit>() {
         val topLevelSymbolList = ArrayList<DocumentSymbol>()
         private val stack = ArrayStack<DocumentSymbol>()
 
-        override fun visitFunctionDeclaration(ctx: FunctionDeclarationContext): DocumentSymbol {
-            return enter(ctx, ctx.simpleName(), SymbolKind.Function)
+        override fun visitFunctionDeclaration(ctx: FunctionDeclarationContext) {
+            enterSymbol(ctx, ctx.simpleName(), SymbolKind.Function)
         }
 
-        override fun visitClassDeclaration(ctx: ClassDeclarationContext): DocumentSymbol {
-            return enter(ctx, ctx.simpleNameOrPrimitiveType(), SymbolKind.Class)
+        override fun visitClassDeclaration(ctx: ClassDeclarationContext) {
+            enterSymbol(ctx, ctx.simpleName() ?: ctx.simpleNameOrPrimitiveType(), SymbolKind.Class)
         }
 
-        override fun visitExpandFunctionDeclaration(ctx: ExpandFunctionDeclarationContext): DocumentSymbol {
-            return enter(ctx, ctx.simpleName(), SymbolKind.Function)
+        override fun visitExpandFunctionDeclaration(ctx: ExpandFunctionDeclarationContext) {
+            enterSymbol(ctx, ctx.simpleName(), SymbolKind.Function)
         }
 
-        override fun visitConstructorDeclaration(ctx: ConstructorDeclarationContext): DocumentSymbol {
-            return enter(ctx, ctx.ZEN_CONSTRUCTOR(), SymbolKind.Constructor)
+        override fun visitConstructorDeclaration(ctx: ConstructorDeclarationContext) {
+            enterSymbol(ctx, ctx.ZEN_CONSTRUCTOR(), SymbolKind.Constructor)
         }
 
-        override fun visitVariableDeclaration(ctx: VariableDeclarationContext): DocumentSymbol {
-            return enter(ctx, ctx.simpleName(), SymbolKind.Variable)
+        override fun visitVariableDeclaration(ctx: VariableDeclarationContext) {
+            enterSymbol(ctx, ctx.simpleName(), SymbolKind.Variable)
         }
 
-        override fun visitFunctionExpr(ctx: FunctionExprContext): DocumentSymbol {
-            return enter(ctx, ctx.FUNCTION(), SymbolKind.Function)
+        override fun visitFunctionExpr(ctx: FunctionExprContext) {
+            enterSymbol(ctx, ctx.FUNCTION(), SymbolKind.Function)
         }
 
-        private fun enter(enclose: RuleNode, selection: ParseTree, kind: SymbolKind): DocumentSymbol {
-            val symbol = toDocumentSymbol(enclose, selection, kind)
-            if (push(symbol)) {
-                visitChildren(enclose)
+        private fun enterSymbol(
+            encloseCtx: RuleNode,
+            selectionCtx: ParseTree?,
+            kind: SymbolKind,
+            name: String? = selectionCtx?.text
+        ) {
+            selectionCtx ?: return
+            name ?: return
+            toDocumentSymbol(encloseCtx, selectionCtx, name, kind).let {
+                push(it)
+                visitChildren(encloseCtx)
                 pop()
             }
-            return symbol
-        }
-
-        private fun toDocumentSymbol(enclose: ParseTree, selection: ParseTree, kind: SymbolKind): DocumentSymbol {
-            return toDocumentSymbol(enclose, selection, selection.text, kind)
         }
 
         private fun toDocumentSymbol(
-            enclose: ParseTree,
-            selection: ParseTree,
+            encloseCtx: ParseTree,
+            selectionCtx: ParseTree,
             name: String,
             kind: SymbolKind
         ): DocumentSymbol {
-            val encloseRange = enclose.textRange.toLspRange()
-            val selectionRange = selection.textRange.toLspRange()
+            val encloseRange = encloseCtx.textRange.toLspRange()
+            val selectionRange = selectionCtx.textRange.toLspRange()
             return DocumentSymbol(name, kind, encloseRange, selectionRange)
         }
 
-        private fun push(symbol: DocumentSymbol): Boolean {
-            if (symbol.name.isEmpty()) {
-                return false
-            }
+        private fun push(symbol: DocumentSymbol) {
             if (isTopLevel) {
                 addToTopLevelSymbolList(symbol)
             } else {
                 addToCurrentSymbolChildren(symbol)
             }
             stack.push(symbol)
-            return true
         }
 
         private fun pop() {
@@ -105,4 +102,3 @@ object DocumentSymbolProvider {
         }
     }
 }
-
