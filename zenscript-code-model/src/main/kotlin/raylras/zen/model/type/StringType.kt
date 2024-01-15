04b4ec1d9d565e1643da8b510247ceecfde45589
raylras <raylras@outlook.com>
2024-01-15 10:33:59 +0800

    fix(StringType): add built-in operator `~`

--- zenscript-code-model/src/main/kotlin/raylras/zen/model/type/StringType.kt
+++ zenscript-code-model/src/main/kotlin/raylras/zen/model/type/StringType.kt
@@ -1,18 +1,25 @@
 package raylras.zen.model.type
 
 import raylras.zen.model.CompilationEnvironment
+import raylras.zen.model.symbol.Operator
 import raylras.zen.model.symbol.Symbol
 import raylras.zen.model.symbol.SymbolProvider
+import raylras.zen.model.symbol.symbolSequence
 
 object StringType : Type, SymbolProvider {
     override val typeName = "string"
 
     override fun getSymbols(env: CompilationEnvironment?): Sequence<Symbol> {
+        val builtIn = symbolSequence {
+            operator(Operator.CONCAT, StringType) {
+                parameter("other", AnyType)
+            }
+        }
         val classDeclared = env?.classes
             ?.filter { it.qualifiedName == typeName }
             ?.flatMap { it.getSymbols() }
             ?: emptySequence()
-        return classDeclared + getExpands(env)
+        return builtIn + classDeclared + getExpands(env)
     }
 
     override fun toString() = typeName
