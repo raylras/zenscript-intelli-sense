7c5d450bb524d9973c5ccd2692b8f7f064a7b47e
raylras <raylras@outlook.com>
2024-01-18 10:50:45 +0800

    refactor(DefinitionProvider): improved implementation; should return an empty list instead of null, close #107

--- zenscript-language-server/src/main/kotlin/raylras/zen/lsp/provider/DefinitionProvider.kt
+++ zenscript-language-server/src/main/kotlin/raylras/zen/lsp/provider/DefinitionProvider.kt
@@ -1,33 +1,69 @@
 package raylras.zen.lsp.provider
 
+import org.antlr.v4.runtime.tree.RuleNode
+import org.antlr.v4.runtime.tree.TerminalNode
 import org.eclipse.lsp4j.DefinitionParams
 import org.eclipse.lsp4j.LocationLink
-import org.eclipse.lsp4j.Range
 import raylras.zen.model.CompilationUnit
+import raylras.zen.model.Visitor
+import raylras.zen.model.parser.ZenScriptParser.*
 import raylras.zen.model.resolve.resolveSymbols
 import raylras.zen.model.symbol.ParseTreeLocatable
 import raylras.zen.model.symbol.Symbol
-import raylras.zen.util.getTerminalAt
-import raylras.zen.util.textRange
-import raylras.zen.util.toLspRange
-import raylras.zen.util.toTextPosition
+import raylras.zen.util.*
 
 object DefinitionProvider {
-    fun definition(unit: CompilationUnit, params: DefinitionParams): List<LocationLink>? {
+    fun definition(unit: CompilationUnit, params: DefinitionParams): List<LocationLink> {
         val cursor = params.position.toTextPosition()
-        val terminal = unit.parseTree.getTerminalAt(cursor) ?: return null
-        val originSelectionRange = terminal.textRange.toLspRange()
-        return resolveSymbols<Symbol>(terminal, unit)
-            .filter { it is ParseTreeLocatable }
-            .map { toLocationLink(it, originSelectionRange) }
-            .toList()
+        val terminal = unit.parseTree.getTerminalAt(cursor) ?: return emptyList()
+        val visitor = DefinitionVisitor(unit, terminal)
+        unit.accept(visitor)
+        return visitor.result.toList()
+    }
+}
+
+class DefinitionVisitor(private val unit: CompilationUnit, private val terminal: TerminalNode) : Visitor<Unit>() {
+    var result = emptySequence<LocationLink>()
+
+    override fun visitQualifiedName(ctx: QualifiedNameContext) {
+        result = resolveSymbols<Symbol>(ctx, unit)
+            .filterIsInstance<ParseTreeLocatable>()
+            .map { it.toLocationLink(ctx.textRange) }
     }
 
-    private fun toLocationLink(symbol: Symbol, originSelectionRange: Range): LocationLink {
-        val locatable = (symbol as ParseTreeLocatable)
-        val uri = locatable.path.toUri().toString()
-        val range = locatable.textRange.toLspRange()
-        val selectionRange = locatable.simpleNameTextRange.toLspRange()
-        return LocationLink(uri, range, selectionRange, originSelectionRange)
+    override fun visitSimpleNameExpr(ctx: SimpleNameExprContext) {
+        result = resolveSymbols<Symbol>(ctx, unit)
+            .filterIsInstance<ParseTreeLocatable>()
+            .map { it.toLocationLink(ctx.textRange) }
     }
+
+    override fun visitMemberAccessExpr(ctx: MemberAccessExprContext) {
+        when (terminal) {
+            in ctx.simpleName() -> {
+                result = resolveSymbols<Symbol>(ctx, unit)
+                    .filterIsInstance<ParseTreeLocatable>()
+                    .map { it.toLocationLink(ctx.simpleName().textRange) }
+            }
+
+            else -> {
+                visitChildren(ctx)
+            }
+        }
+    }
+
+    override fun visitChildren(node: RuleNode) {
+        for (i in 0 until node.childCount) {
+            val child = node.getChild(i)
+            if (terminal in child) {
+                return child.accept(this)
+            }
+        }
+    }
+}
+
+private fun ParseTreeLocatable.toLocationLink(originSelectionRange: TextRange): LocationLink {
+    val uri = this.path.toUri().toString()
+    val range = this.textRange.toLspRange()
+    val selectionRange = this.simpleNameTextRange.toLspRange()
+    return LocationLink(uri, range, selectionRange, originSelectionRange.toLspRange())
 }
