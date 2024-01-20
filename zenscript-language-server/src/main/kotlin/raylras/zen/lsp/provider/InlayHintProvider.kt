6c6177db8e447c9ccaa87c67f4510d11a056859a
raylras <raylras@outlook.com>
2024-01-20 10:55:38 +0800

    perf(InlayHintProvider): do not provide hints for areas outside the specified range

--- zenscript-language-server/src/main/kotlin/raylras/zen/lsp/provider/InlayHintProvider.kt
+++ zenscript-language-server/src/main/kotlin/raylras/zen/lsp/provider/InlayHintProvider.kt
@@ -1,10 +1,11 @@
 package raylras.zen.lsp.provider
 
+import org.antlr.v4.runtime.tree.RuleNode
 import org.eclipse.lsp4j.InlayHint
 import org.eclipse.lsp4j.InlayHintParams
 import org.eclipse.lsp4j.jsonrpc.messages.Either
 import raylras.zen.model.CompilationUnit
-import raylras.zen.model.Listener
+import raylras.zen.model.Visitor
 import raylras.zen.model.brackets.BracketHandlers
 import raylras.zen.model.parser.ZenScriptParser.*
 import raylras.zen.model.resolve.resolveType
@@ -13,22 +14,30 @@ import raylras.zen.model.symbol.ParameterSymbol
 import raylras.zen.model.symbol.TypeAnnotatable
 import raylras.zen.model.symbol.VariableSymbol
 import raylras.zen.model.type.FunctionType
-import raylras.zen.util.TextPosition
-import raylras.zen.util.textRange
-import raylras.zen.util.toLspPosition
+import raylras.zen.util.*
 
 object InlayHintProvider {
     fun inlayHint(unit: CompilationUnit, params: InlayHintParams): List<InlayHint> {
-        val listener = InlayHintListener(unit)
-        unit.accept(listener)
-        return listener.result
+        val visitor = InlayHintVisitor(unit, params.range.toTextRange())
+        unit.accept(visitor)
+        return visitor.result
     }
 }
 
-class InlayHintListener(private val unit: CompilationUnit) : Listener() {
+class InlayHintVisitor(private val unit: CompilationUnit, private val range: TextRange) : Visitor<Unit>() {
     val result = ArrayList<InlayHint>()
 
-    override fun enterVariableDeclaration(ctx: VariableDeclarationContext) {
+    override fun visitChildren(node: RuleNode) {
+        for (i in 0 until node.childCount) {
+            val child = node.getChild(i)
+            if (child.textRange in range) {
+                child.accept(this)
+            }
+        }
+    }
+
+    override fun visitVariableDeclaration(ctx: VariableDeclarationContext) {
+        if (ctx.textRange !in range) return
         val symbol = unit.symbolMap[ctx]
         when {
             symbol !is VariableSymbol -> return
@@ -36,9 +45,11 @@ class InlayHintListener(private val unit: CompilationUnit) : Listener() {
             symbol.typeAnnotationCst != null -> return
             else -> addInlayHint(symbol.typeAnnotationTextPosition, " as ${symbol.type.simpleTypeName}")
         }
+        visitChildren(ctx)
     }
 
-    override fun enterFunctionDeclaration(ctx: FunctionDeclarationContext) {
+    override fun visitFunctionDeclaration(ctx: FunctionDeclarationContext) {
+        if (ctx.textRange !in range) return
         val symbol = unit.symbolMap[ctx]
         when {
             symbol !is FunctionSymbol -> return
@@ -46,9 +57,11 @@ class InlayHintListener(private val unit: CompilationUnit) : Listener() {
             symbol.typeAnnotationCst != null -> return
             else -> addInlayHint(symbol.typeAnnotationTextPosition, " as ${symbol.returnType.simpleTypeName}")
         }
+        visitChildren(ctx)
     }
 
-    override fun enterFormalParameter(ctx: FormalParameterContext) {
+    override fun visitFormalParameter(ctx: FormalParameterContext) {
+        if (ctx.textRange !in range) return
         val symbol = unit.symbolMap[ctx]
         when {
             symbol !is ParameterSymbol -> return
@@ -58,7 +71,8 @@ class InlayHintListener(private val unit: CompilationUnit) : Listener() {
         }
     }
 
-    override fun enterForeachVariable(ctx: ForeachVariableContext) {
+    override fun visitForeachVariable(ctx: ForeachVariableContext) {
+        if (ctx.textRange !in range) return
         val symbol = unit.symbolMap[ctx]
         when {
             symbol !is VariableSymbol -> return
@@ -68,7 +82,8 @@ class InlayHintListener(private val unit: CompilationUnit) : Listener() {
         }
     }
 
-    override fun enterFunctionExpr(ctx: FunctionExprContext) {
+    override fun visitFunctionExpr(ctx: FunctionExprContext) {
+        if (ctx.textRange !in range) return
         when {
             ctx.typeLiteral() != null -> return
             else -> {
@@ -77,9 +92,10 @@ class InlayHintListener(private val unit: CompilationUnit) : Listener() {
                 }
             }
         }
+        visitChildren(ctx)
     }
 
-    override fun enterBracketHandlerExpr(ctx: BracketHandlerExprContext) {
+    override fun visitBracketHandlerExpr(ctx: BracketHandlerExprContext) {
         val expr = ctx.raw().text
         BracketHandlers.getLocalizedNameLocal(expr, unit.env)?.let { name ->
             addInlayHint(ctx.textRange.end, ": $name")
