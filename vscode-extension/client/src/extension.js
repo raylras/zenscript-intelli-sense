09698eaca02716383f753a50f3138b68f06dadc6
raylras <raylras@outlook.com>
2024-01-30 22:51:06 +0800

    fix(vscode-extension): fix `findJavaExecutable()` #115

--- vscode-extension/client/src/extension.js
+++ vscode-extension/client/src/extension.js
@@ -13,20 +13,34 @@ let languageClient = undefined;
 export async function activate(context) {
     registerGeneratedSourcesView();
 
-    let javaBin = workspace.getConfiguration().get('zenscript.languageServer.javaBin');
-    if (!javaBin) {
-        LocateJavaHome({version: ">=11"}, (error, javaHomes) => {
-            if (javaHomes.length === 0) {
-                window.showErrorMessage("No valid Java environment found, please install Java 11 or later");
-                return
-            }
-            javaBin = javaHomes[0].executables.java;
-        });
-    }
-
-    languageClient = await activateLanguageServer(javaBin);
+    findJavaExecutable().then(javaBin => {
+        activateLanguageServer(javaBin).then(client => {
+            languageClient = client;
+        })
+    })
 }
 
 export async function deactivate() {
     return languageClient?.stop();
+}
+
+/**
+ * @returns {Promise<string>}
+ */
+function findJavaExecutable() {
+    return new Promise((resolve, reject) => {
+        let javaBin = workspace.getConfiguration().get('zenscript.languageServer.javaBin');
+        if (javaBin !== undefined && javaBin !== '') {
+            resolve(javaBin);
+        } else {
+            LocateJavaHome({version: ">=11"}, (error, javaHomes) => {
+                if (javaHomes.length === 0) {
+                    window.showErrorMessage("No valid Java environment found, please install Java 11 or later");
+                    reject();
+                } else {
+                    resolve(javaHomes[0].executables.java);
+                }
+            });
+        }
+    });
 }
