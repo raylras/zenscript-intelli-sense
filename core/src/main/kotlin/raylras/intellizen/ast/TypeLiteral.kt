93bf395622223f2e11fd00fe4eab27e4f8f3e619
raylras <raylras@outlook.com>
2024-06-28 03:00:46 +0800

    Register some node mapper

--- core/src/main/kotlin/raylras/intellizen/ast/TypeLiteral.kt
+++ core/src/main/kotlin/raylras/intellizen/ast/TypeLiteral.kt
@@ -2,33 +2,33 @@ package raylras.intellizen.ast
 
 import com.strumenta.kolasu.model.Node
 
-sealed interface TypeLiteral {
-    fun asString(): String
+sealed class TypeLiteral : Node() {
+    abstract fun asString(): String
 }
 
 data class ArrayTypeLiteral(
     val baseType: TypeLiteral
-) : Node(), TypeLiteral {
+) : TypeLiteral() {
     override fun asString() = baseType.asString() + "[]"
 }
 
 data class ListTypeLiteral(
     val baseType: TypeLiteral
-) : Node(), TypeLiteral {
+) : TypeLiteral() {
     override fun asString() = "[" + baseType.asString() + "]"
 }
 
 data class MapTypeLiteral(
     val keyType: TypeLiteral,
     val valueType: TypeLiteral,
-) : Node(), TypeLiteral {
+) : TypeLiteral() {
     override fun asString() = valueType.asString() + "[" + keyType.asString() + "]"
 }
 
 data class FunctionTypeLiteral(
     val parameterTypes: List<TypeLiteral>,
     val returnType: TypeLiteral,
-) : Node(), TypeLiteral {
+) : TypeLiteral() {
     override fun asString() = parameterTypes.joinToString(
         separator = ",",
         prefix = "function(",
@@ -38,24 +38,24 @@ data class FunctionTypeLiteral(
 
 data class ReferenceTypeLiteral(
     val typeName: String,
-) : Node(), TypeLiteral {
+) : TypeLiteral() {
     override fun asString() = typeName
 }
 
 data class UnionTypeLiteral(
     val subTypes: List<TypeLiteral>
-) : Node(), TypeLiteral {
+) : TypeLiteral() {
     override fun asString() = subTypes.joinToString(" | ")
 }
 
 data class IntersectionTypeLiteral(
     val subTypes: List<TypeLiteral>
-) : Node(), TypeLiteral {
+) : TypeLiteral() {
     override fun asString() = subTypes.joinToString(" & ")
 }
 
 data class PrimitiveTypeLiteral(
     val typeName: String,
-) : Node(), TypeLiteral {
+) : TypeLiteral() {
     override fun asString() = typeName
 }
--- core/src/main/kotlin/raylras/intellizen/parser/ZenScript.kt
+++ core/src/main/kotlin/raylras/intellizen/parser/ZenScript.kt
@@ -1,10 +1,6 @@
 package raylras.intellizen.parser
 
-import com.strumenta.kolasu.mapping.ParseTreeToASTTransformer
-import com.strumenta.kolasu.mapping.translateCasted
-import com.strumenta.kolasu.mapping.translateList
-import com.strumenta.kolasu.mapping.translateOnlyChild
-import com.strumenta.kolasu.mapping.translateOptional
+import com.strumenta.kolasu.mapping.*
 import com.strumenta.kolasu.model.Node
 import com.strumenta.kolasu.model.Source
 import com.strumenta.kolasu.model.debugPrint
@@ -14,12 +10,10 @@ import com.strumenta.kolasu.parsing.KolasuParser
 import com.strumenta.kolasu.validation.Issue
 import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.Lexer
+import org.antlr.v4.runtime.Token
 import org.antlr.v4.runtime.TokenStream
-import raylras.intellizen.ast.CompilationUnit
-import raylras.intellizen.ast.Identifier
-import raylras.intellizen.ast.ImportDeclaration
+import raylras.intellizen.ast.*
 import raylras.intellizen.parser.ZenScriptParser.*
-import raylras.intellizen.parser.ZenScriptParser.CompilationUnitContext
 
 class ZenScriptKolasuParser :
     KolasuParser<CompilationUnit, ZenScriptParser, CompilationUnitContext, KolasuANTLRToken>(ANTLRTokenFactory()) {
@@ -73,18 +67,103 @@ class ZenScriptKolasuMapper(
                 text = text
             )
         }
+        registerNodeFactory<VariableDeclarationContext, VariableDeclaration> {
+            VariableDeclaration(
+                declaringKind = declaringKind(prefix),
+                identifier = translateCasted(simpleIdentifier()),
+                typeLiteral = translateOptional(type()),
+                initializer = translateOptional(expression())
+            )
+        }
+        registerNodeFactory<FunctionDeclarationContext, FunctionDeclaration> {
+            FunctionDeclaration(
+                declaringKind = declaringKind(prefix),
+                identifier = translateOptional(simpleIdentifier()),
+                parameters = functionValueParameters().functionValueParameter().map { translateCasted(it) },
+                returnTypeLiteral = translateOptional(type()),
+                body = block().statement().map { translateCasted(it) }
+            )
+        }
+        registerNodeFactory<ExpandFunctionDeclarationContext, ExpandFunctionDeclaration> {
+            ExpandFunctionDeclaration(
+                receiverTypeLiteral = translateCasted(type()),
+                identifier = translateCasted(simpleIdentifier()),
+                parameters = functionValueParameters().functionValueParameter().map { translateCasted(it) },
+                returnTypeLiteral = translateOptional(type()),
+                body = block().statement().map { translateCasted(it) }
+            )
+        }
+        registerNodeFactory<FunctionValueParameterContext, ParameterDeclaration> {
+            ParameterDeclaration(
+                identifier = translateCasted(simpleIdentifier()),
+                typeLiteral = translateOptional(type()),
+                defaultValue = translateOptional(expression())
+            )
+        }
+        registerNodeFactory<ClassDeclarationContext, ClassDeclaration> {
+            ClassDeclaration(
+                identifier = translateCasted(simpleIdentifier()),
+                classBodyEntities = classBody().classMemberDeclaration().map { translateOnlyChild(it) }
+            )
+        }
+        registerNodeFactory<ConstructorDeclarationContext, ConstructorDeclaration> {
+            ConstructorDeclaration(
+                parameters = functionValueParameters().functionValueParameter().map { translateCasted(it) },
+                body = block().statement().map { translateCasted(it) }
+            )
+        }
+
+        //region TypeLiteral
+        registerNodeFactory<TypeContext, TypeLiteral> {
+            translateOnlyChild(this)
+        }
+        registerNodeFactory<FunctionTypeContext, FunctionTypeLiteral> {
+            FunctionTypeLiteral(
+                parameterTypes = translateList(parameters),
+                returnType = translateCasted(returnType)
+            )
+        }
+        registerNodeFactory<ParenthesizedTypeContext, TypeLiteral> {
+            translateOnlyChild(this)
+        }
+        registerNodeFactory<ListTypeContext, ListTypeLiteral> {
+            ListTypeLiteral(
+                baseType = translateCasted(type())
+            )
+        }
+        registerNodeFactory<ArrayTypeContext, ArrayTypeLiteral> {
+            ArrayTypeLiteral(
+                baseType = translateCasted(type())
+            )
+        }
+        registerNodeFactory<MapTypeContext, MapTypeLiteral> {
+            MapTypeLiteral(
+                keyType = translateCasted(key),
+                valueType = translateCasted(value)
+            )
+        }
+        registerNodeFactory<PrimitiveTypeContext, PrimitiveTypeLiteral> {
+            PrimitiveTypeLiteral(
+                typeName = text
+            )
+        }
+        //endregion
 
         // TODO
     }
 }
 
+fun declaringKind(token: Token?): DeclaringKind = when (token?.type) {
+    VAR -> DeclaringKind.VAR
+    VAL -> DeclaringKind.VAL
+    STATIC -> DeclaringKind.STATIC
+    GLOBAL -> DeclaringKind.GLOBAL
+    else -> DeclaringKind.NONE
+}
+
 fun main() {
     val code = """
-        A.foo
-        
-        zenClass A {
-            static foo as int;
-        }
+        static foo as
     """.trimIndent()
     val parser = ZenScriptKolasuParser()
     val result = parser.parse(code)
